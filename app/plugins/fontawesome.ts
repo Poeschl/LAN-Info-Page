c0e600f36dcf0017701975fb9864390a0dd9911b
Markus Pöschl <Poeschl@users.noreply.github.com>
2026-07-31 20:17:02 +0200

    :lipstick: Refactor FontAwesome icon imports to add all icons at once

--- app/plugins/fontawesome.ts
+++ app/plugins/fontawesome.ts
@@ -1,45 +1,16 @@
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faArrowRightFromBracket,
-  faBook,
-  faChartLine,
-  faCircleNotch,
-  faFile,
-  faFileArrowDown,
-  faFileZipper,
-  faGear,
-  faHeadset,
-  faHome,
-  faScrewdriverWrench,
-  faServer,
-  faTriangleExclamation,
-  faTrophy,
-} from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
-// You can add your icons directly in this plugin. See other examples for how you
-// can add other styles or just individual icons.
-library.add(faGithub);
-library.add(faCircleNotch);
-library.add(faTriangleExclamation);
-library.add(faHome);
-library.add(faBook);
-library.add(faChartLine);
-library.add(faUser);
-library.add(faArrowRightFromBracket);
-library.add(faHeadset);
-library.add(faScrewdriverWrench);
-library.add(faServer);
-library.add(faTrophy);
-library.add(faFileArrowDown);
-library.add(faFileZipper);
-library.add(faGear);
-library.add(faFile);
+// Add all icons, since the user has the options select a icon for the links at runtime.
+library.add(fas);
+library.add(far);
+library.add(fab);
 
 // noinspection JSUnusedGlobalSymbols
 export default defineNuxtPlugin((nuxtApp) => {
